75e4951d17f4d26827f0ab1af25ed3e985aa70ad
NikolaosChA <n.ch.alex.95@gmail.com>
2022-06-04 13:29:48 +0000

    HELP

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     path(route='', view=views.get_dealerships, name='index'),
     
     # path for add a review view
-    path(route='dealer/<int:id>/review', view=views.add_review, name='add_review')
+    path(route='dealer/<int:id>/review', view=views.add_review, name='add_review'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
